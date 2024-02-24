71732b07a52663a3c540226408fe61b264e94835
wirbel <womp@herr-der-mails.de>
2024-02-24 15:01:47 +0100

    update HISTORY && prepare for tag 20240224

--- satip.c
+++ satip.c
@@ -25,7 +25,7 @@
 #define GITVERSION ""
 #endif
 
-       const char VERSION[]     = "20240221" GITVERSION;
+       const char VERSION[]     = "20240224" GITVERSION;
 static const char DESCRIPTION[] = trNOOP("SAT>IP Devices");
 
 
